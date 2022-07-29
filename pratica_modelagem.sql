ef061cff84243438c674865ee92d576fd09bf6c6
Arthur Akira <arthurak10.at@gmail.com>
2022-07-29 20:49:32 -0300

    initial commit

---
+++ pratica_modelagem.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE drivenbank();
+
+CREATE TABLE states (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE cities(
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	"stateId" INTEGER NOT NULL REFERENCES states(id)
+);
+
+CREATE TABLE customers(
+	id SERIAL PRIMARY KEY,
+	"fullName" TEXT NOT NULL,
+	cpf VARCHAR(11) UNIQUE NOT NULL,
+	email VARCHAR(30) NOT NULL,
+	password TEXT NOT NULL
+);
+
+CREATE TABLE "customerPhones"(
+	id SERIAL PRIMARY KEY,
+	"customerId" INTEGER NOT NULL REFERENCES customers(id),
+	number INTEGER UNIQUE NOT NULL,
+	type TEXT NOT NULL DEFAULT 'mobile'
+);
+
+CREATE TABLE "customerAddresses"(
+	id SERIAL PRIMARY KEY,
+	"customerId" INTEGER UNIQUE NOT NULL REFERENCES customers(id),
+	street VARCHAR(30) NOT NULL,
+	number INTEGER NOT NULL,
+	complement VARCHAR(30),
+	"postalCode" VARCHAR(10) NOT NULL,
+	"cityId" INTEGER UNIQUE NOT NULL REFERENCES cities(id)
+);
+
+CREATE TABLE "bankAccount"(
+	id SERIAL PRIMARY KEY,
+	"customerId" INTEGER NOT NULL REFERENCES customers(id),
+	"accountNumber" INTEGER NOT NULL,
+	agency INTEGER NOT NULL,
+	"openDate" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"closeDate" TIMESTAMP DEFAULT NULL
+);
